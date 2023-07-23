810e44c87359e1b668c510cf61b00425ab3c9073
Douglas Ciro de Campos <116591018+camposciro@users.noreply.github.com>
2023-07-23 18:12:37 -0300

    RPG0008  - Meu primeiro site "cringe"
    Missão Prática | Nível 1 | Mundo 2

---
+++ Receitas/scripts.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+});
